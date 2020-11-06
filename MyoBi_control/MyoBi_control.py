c8b8c35d372863ee1d2fa5dccfaba6f57d1db04b
Lauge Øster Vinther <lauge.oester.vinther@post.au.dk>
2020-11-06 11:46:44 +0100

    Longer sleep period

--- MyoBi_control/MyoBi_control.py
+++ MyoBi_control/MyoBi_control.py
@@ -31,13 +31,13 @@ try:
 					hC.sendCommand(b'F0 P100 \n\r F1 P100 \n\r F2 P0 \n\r F3 P100 \n\r')
 					gripDone = True
 					Voltage.clear()
-					time.sleep(1)
+					time.sleep(5)
 					print('Spænding under 2.5')
 				else:
 					hC.sendCommand(b'F0 P0 \n\r F1 P0 \n\r F2 P0 \n\r F3 P0 \n\r')
 					gripDone = False
 					Voltage.clear()
-					time.sleep(1)
+					time.sleep(5)
 					print('Spænding over 2.5')
 except KeyboardInterrupt:
 	pass
