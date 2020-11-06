9936e0c6b6c130c555714e2072bfc00f79fc4e6b
Lauge Øster Vinther <lauge.oester.vinther@post.au.dk>
2020-11-06 11:49:47 +0100

    Moved print method

--- MyoBi_control/MyoBi_control.py
+++ MyoBi_control/MyoBi_control.py
@@ -30,18 +30,19 @@ try:
 				if(gripDone == False):
 					hC.sendCommand(b'F0 P100 \n\r F1 P100 \n\r F2 P0 \n\r F3 P100 \n\r')
 					gripDone = True
+					print(str(statistics.mean(Voltage)))
 					Voltage.clear()
 					time.sleep(5)
 					print('Spænding under 2.5')
-					print(str(statistics.mean(Voltage)))
 
 				else:
 					hC.sendCommand(b'F0 P0 \n\r F1 P0 \n\r F2 P0 \n\r F3 P0 \n\r')
 					gripDone = False
+					print(str(statistics.mean(Voltage)))
 					Voltage.clear()
 					time.sleep(5)
 					print('Spænding over 2.5')
-					print(str(statistics.mean(Voltage)))
+					
 
 except KeyboardInterrupt:
 	pass
