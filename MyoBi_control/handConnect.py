56c61ffd3d5598eb8d165994b0f565152808a4ea
Lauge Øster Vinther <lauge.oester.vinther@post.au.dk>
2020-11-17 10:14:34 +0100

    No encoding (again)

--- MyoBi_control/handConnect.py
+++ MyoBi_control/handConnect.py
@@ -3,4 +3,4 @@ import serial
 hand = serial.Serial('/dev/serial/by-path/platform-3f980000.usb-usb-0:1.3:1.0', 115200, timeout=1)
 
 def sendCommand(command):
-    hand.write(command.encode('ascii'))
+    hand.write(command)
